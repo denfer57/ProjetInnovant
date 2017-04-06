4944885041dc65c5f57c7b56b26c21b1a204c985
denfer57 <jordan.fromeyer@gmail.com>
2017-04-06 12:19:40 +0200

    backend event
    Modification backend event

--- Now/src/pages/event-form/event-form.ts
+++ Now/src/pages/event-form/event-form.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {Http} from '@angular/http';
 import { ToastController } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
+/*import { Camera } from '@ionic-native/camera';
+import { Transfer, FileUploadOptions, TransferObject } from '@ionic-native/transfer';*/
 
+var latitude, longitude;
 /*
   Generated class for the EventForm page.
-
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
@@ -16,19 +19,91 @@ import { ToastController } from 'ionic-angular';
 export class EventFormPage {
 	
 	data:any;
+	//base64Image:string;
+	//toto: string;
 	
-  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, toastCtrl: ToastController/*, private transfer: Transfer,private camera: Camera*/) {
     this.data = {};
+	this.recupererLatLng();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventFormPage');
   }
+  
+  public recupererLatLng(){
+	let locationOptions = {
+			enableHighAccuracy: true,
+			timeout: 20000,
+			maximumAge: 0
+	};
+
+	// Get user's position
+	Geolocation.getCurrentPosition(locationOptions).then((resp) => {
+		latitude = resp.coords.latitude;
+		longitude = resp.coords.longitude;
+	}).catch((error) => {
+				console.log('Error getting location', error);
+	});
+	return latitude, longitude;
+  }
+  
+  /*public recupererAdresse(latitude,longitude){
+	si on a le temps, récupérer le formatted address en JSON
+	this.http.get(https://maps.googleapis.com/maps/api/geocode/json?latlng=latitude,longitude); 
+  }*/
+  
+  /*prendre une photo et l'uploader
+  upload()
+  {
+    let options = {
+		quality: 100
+    };
+	
+    this.camera.getPicture(options).then((imageData) => {
+    // imageData is either a base64 encoded string or a file URI
+    // If it's base64:
+	
+		const fileTransfer: TransferObject = this.transfer.create();
+		  let options1: FileUploadOptions = {
+			 fileKey: 'file',
+			 fileName: 'name.jpg',
+			 headers: {}
+		}
+		fileTransfer.upload(imageData, 'http://ionicserver:8888/ProjetInnovant/server/app/Controllers/Events-Controller.php', options1)
+		.then((data) => {
+			// success
+			alert("success");
+		}, (err) => {
+			// error
+			alert("error"+JSON.stringify(err));
+		});
+    });
+  }*/
+  
+  /*upload(){
+
+  this.camera.getPicture({
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType     : this.camera.PictureSourceType.PHOTOLIBRARY,
+      mediaType: this.camera.MediaType.PICTURE
+  }).then((imageData) => {
+    // imageData is a base64 encoded string
+	this.toto = imageData;
+	this.base64Image = "data:image/jpeg;base64," + imageData; //this.toto;
+	console.log("Ok");
+  }, (err) => {
+      console.log(err);
+  });
+  }*/
 
   public submit(){
 	var token = window.localStorage.getItem('token');
     var link = 'http://ionicserver:8888/api/events';
-    this.http.post(link, {token, name : this.data.name, categorie : this.data.categorie, description : this.data.description} ).subscribe(function (res){
+	//console.log(latitude);
+	
+	//this.recupererAdresse();
+    this.http.post(link, {latitude, longitude, token, name : this.data.name, categorie : this.data.categorie, description : this.data.description} ).subscribe(function (res){
       if(JSON.parse(res["_body"]).erreur==null){
         console.log("pas d'erreur");
         document.getElementById("rep").innerHTML="The event "+JSON.parse(res["_body"]).name+" has been registered";
@@ -48,6 +123,6 @@ export class EventFormPage {
        var str = text.value;
        str = str.substring(0, str.length - 1);
        text.value = str;
-     }
+    }
    }
 }
--- server/app/Controllers/EventsController.php
+++ server/app/Controllers/EventsController.php
@@ -4,11 +4,50 @@ namespace App\Controllers;
 use Pragma\Controller\BaseController;
 use App\Models\Event;
 use App\Models\User;
+//header('Access-Control-Allow-Origin: *');
 
 class EventsController extends BaseController{
+    /*public function upload($index,$destination,$maxsize=FALSE,$extensions=FALSE)
+    {
+      //Test1: fichier correctement uploadé
+      if (!isset($_FILES[$index]) OR $_FILES[$index]['error'] > 0) return FALSE;
+      //Test2: taille limite
+      if ($maxsize !== FALSE AND $_FILES[$index]['size'] > $maxsize) return FALSE;
+      //Test3: extension
+      $ext = substr(strrchr($_FILES[$index]['name'],'.'),1);
+      if ($extensions !== FALSE AND !in_array($ext,$extensions)) return FALSE;
+      //Déplacement
+      return move_uploaded_file($_FILES[$index]['tmp_name'],$destination);
+    }*/
+
     public function create(){
       $eventOK=true;
+      $imageOK=true;
       $erreur="";
+      $token = $this->params["token"];
+      $u = User::forge()->where("token", "=", $token)->first();
+
+      /*Base64.decode64(image_data[:content]);
+      $target_path = "uploads/";
+      $target_path .= $target_path . basename( $_FILES['file']['name']);
+
+      if (move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+        echo "Upload and move success";
+      } else {
+        echo $target_path;
+        echo "There was an error uploading the file, please try again!";
+      }*/
+
+      //Récupère les infos de l'image
+      //$nomFichierBase = $_FILES["photo"]["name"];
+      //$nomFichierTemp = $_FILES["photo"]["tmp_name"];
+      //$typeFichier = pathinfo($nomFichierBase, PATHINFO_EXTENSION);
+      //$id = uniqid();
+      //$upload = upload('photo','Images/event_$id',1048576, array('png','gif','jpg','jpeg') );
+
+      /*if(empty($_POST['photo'])){
+        $erreur.="There is no picture. <br>";
+      }*/
       if(empty($this->params['name'])){
         $erreur.= "Please enter a name for your event. <br>";
         $eventOK=false;
@@ -23,17 +62,17 @@ class EventsController extends BaseController{
       }
 
       if ($eventOK) {
-        $token = $this->params["token"];
-        $u = User::forge()->where("token", "=", $token)->first();
-
         $e = Event::build([
+            //'picture' => $nomFichierFinal,
+            'latitude' => $this->params["latitude"],
+            'longitude' => $this->params["longitude"],
             'user_id' => $u->id,
             'name' => $this->params["name"],
             'categorie' => $this->params["categorie"],
             'description' => $this->params["description"]
         ]);
         $e->save();
-
+        
         echo json_encode(array('token'=>$token,'name'=>$this->params["name"],'categorie'=>$this->params["categorie"], 'description'=>$this->params["description"]));
       }
       else{
@@ -43,23 +82,16 @@ class EventsController extends BaseController{
 
     public function delete(){
       /*
-      Récuperer le token ?
       Voir pourquoi le delete marche pas, à cause de l'user_id ?
       $token = "58dabf6db7c0e";
       $u = User::forge()->where("token", "=", $token)->first();
-      $e = Event::forge()->where("user_id", "=", $u->user_id)->first();
+      $e = Event::forge()->where("user_id", "=", $u->id)->all();
       $e->delete();
       */
     }
 
     public function load(){
-      /*
-      Récuperer le token ?
-      $token = "58dabf6db7c0e";
-      $u = User::forge()->where("token", "=", $token)->first();
-      $e = Event::forge()->where("user_id", "=", $u->user_id)->first();
-      $e->all();
-      */
+      //$e = Event::forge()->select();
     }
 
     public function update(){
